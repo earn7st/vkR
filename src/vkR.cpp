00acd2ce85420790d1cc736837f862f892c9574f
earn <earnstw7@gmail.com>
2026-05-31 01:35:36 +0800

    Hello, Vulkan!

--- src/vkR.cpp
+++ src/vkR.cpp
@@ -1,8 +1,30 @@
-
-
 #include <iostream>
+#include <assert.h>
 
+#include <GLFW/glfw3.h>
+
+#if 0
+#include <vulkan/vulkan.h>
+#else
+#include <vulkan/vulkan.hpp>
+#endif
 int main()
 {
+
+	int rc = glfwInit();
+	assert(rc == GLFW_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "vkR", nullptr, nullptr);  
+	assert(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+
+
     std::cout << "Hello World!\n";
 }
