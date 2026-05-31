00dcc7ebadbbbc31447d126f9838dc16c55fd858
earn <earnstw7@gmail.com>
2026-05-31 12:12:12 +0800

    Using SDL and Volk, Remove glfw

--- src/vkR.cpp
+++ src/vkR.cpp
@@ -1,29 +1,85 @@
 #include <iostream>
 #include <assert.h>
 
-#include <GLFW/glfw3.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
 
-#if 0
+#define VOLK_IMPLEMENTATION
+#include <volk/volk.h>
+
+#if 1
 #include <vulkan/vulkan.h>
 #else
 #include <vulkan/vulkan.hpp>
 #endif
+
+#define VK_CHECK(call) \
+	do { \
+		VkResult result_ = call; \
+		assert(result_ == VK_SUCCESS); \
+	} while (0)
+
+#define CHECK(call) \
+	do { \
+		auto result_ = call; \
+		assert(result_); \
+	} while (0)
+
 int main()
 {
 
-	int rc = glfwInit();
-	assert(rc == GLFW_TRUE);
+	CHECK(SDL_Init(SDL_INIT_VIDEO));
+	VK_CHECK(volkInitialize());
 
-	GLFWwindow* window = glfwCreateWindow(1024, 768, "vkR", nullptr, nullptr);  
+	
+	VkApplicationInfo appInfo {
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pNext = nullptr,
+		.pApplicationName = "vkR",
+		.applicationVersion = VK_MAKE_VERSION(0, 1, 0),
+		.pEngineName = "vkR Engine",
+		.engineVersion = VK_MAKE_VERSION(0, 1, 0),
+		.apiVersion = VK_API_VERSION_1_4,
+	};
+
+	uint32_t instanceExtensionsCount;
+	char const* const* instanceExtensions{ SDL_Vulkan_GetInstanceExtensions(&instanceExtensionsCount) };
+
+	VkInstanceCreateInfo instanceCreateInfo {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pNext = nullptr,
+		.pApplicationInfo = &appInfo,
+		.enabledExtensionCount = instanceExtensionsCount,
+		.ppEnabledExtensionNames = instanceExtensions,
+	};
+
+	VkInstance instance;
+	VK_CHECK(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
+	volkLoadInstance(instance);
+
+	VkSurfaceKHR surface;
+	struct {
+		int x, y;
+	} windowSize;
+	SDL_Window* window = SDL_CreateWindow("vkR", 1280u, 720u, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
 	assert(window);
+	CHECK(SDL_Vulkan_CreateSurface(window, instance, nullptr, &surface));
+	CHECK(SDL_GetWindowSize(window, &windowSize.x, &windowSize.y));
 
-	while (!glfwWindowShouldClose(window))
+	bool running = true;
+
+	while (running)
 	{
-		glfwPollEvents();
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_EVENT_QUIT)
+			{
+				running = false;
+			}
+		}
 	}
-
-	glfwDestroyWindow(window);
-
 
 
     std::cout << "Hello World!\n";
